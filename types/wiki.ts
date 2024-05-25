5f21f9c613f7ca3b218528753195194e2255f8aa
ivanp <ivanp@getamplifylife.com>
2024-05-25 17:40:05 +0300

    feat: wiki constans and enums

---
+++ types/wiki.ts
@@ -0,0 +1,22 @@
+enum Language {
+    Bengali = 'bn',
+    German = 'de',
+    Greek = 'el',
+    English = 'en',
+    Hebrew = 'he',
+    Hungarian = 'hu',
+    Japanese = 'ja',
+    Latin = 'la',
+    Sindhi = 'sd',
+    Swedish = 'sv',
+    Urdu = 'ur',
+    Chinese = 'zh',
+}
+
+const LANG_LIST: Array<{ name: string, value: string }> = []
+
+for (const [key, value] of Object.entries(Language)) {
+    LANG_LIST.push({ name: key, value })
+}
+
+export { LANG_LIST, Language };
